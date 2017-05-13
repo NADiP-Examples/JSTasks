6b87057e60201b7e6120ce002f5b81aa6b5d3335
booblegum <booblegum42@gmail.com>
2017-05-13 19:00:50 +0700

    init project

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function sendData(form) {
+    console.log('form = ', JSON.stringify($(form).serializeArray()));
+    return false
+}
+
+function addPhone() {
+    console.log('add Phone');
+//    ...
+}
